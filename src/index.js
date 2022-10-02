23ed93a06f325ed136ad2eacf67671e7606bbe83
madelinedulchin <madeline.dulchin@gmail.com>
2022-10-02 13:38:45 -0400

    commit tree 8f90dccfc9d74a873cb4a2216076df443a7344e5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+// const dogContainer = document.getElementById('dog-image-container')
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => renderImages(data))
+    
+    const dogContainer = document.getElementById('dog-image-container')
+    // const dogImages = document.createElement('img')
+function renderImages(image) {
+    image.message.forEach((img) => {
+       const dogImage = document.createElement('img');
+       dogImage.src = img
+       dogContainer.append(dogImage)
+        
+    })
+}
+fetch(breedUrl)
+.then(res => res.json())
+.then(data => renderBreed(data))
+
+const breedContainer = document.getElementById('dog-breeds')
+
+function renderBreed(breeds) {
+    for(const breed in breeds.message){
+        const breedList = document.createElement('li');
+        breedList.textContent = breed
+        breedContainer.append(breedList)
+        breedList.addEventListener('click', () => {
+            breedList.style.color = "blue"
+        }
+        )
+    }
+}
+
+})
+
+
